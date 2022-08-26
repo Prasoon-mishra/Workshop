25bb1c085316af0685b7a8c608c73d4135a441a9
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-26 23:29:44 +0530

    [Prasoon] Added practce question

---
+++ ConsoleApp1/MonthtlyHrs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class MonthtlyHrs
+    {
+        public abstract void HoursPerMonth();
+
+    }
+    public class Implementatin : MonthtlyHrs
+    {
+        public override void HoursPerMonth()
+        {
+          
+            var Monthly_Hours = new ArrayList();
+            int Is_Full_Time = 1;
+            int Is_Part_Time = 2;
+            int empHrs = 0;
+            int WorkingDays = 20;
+            int dailyWage = 100;
+            int monthlyHours = 0;
+            int num_of_comp = 5;
+            while (num_of_comp > 0)
+            {
+                monthlyHours = 0;
+                for (int i = 0; i <= WorkingDays; i++)
+                {
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 3);
+                    switch (empCheck)
+                    {
+                        case 1:
+                            empHrs = 8;
+                            break;
+                        case 2:
+                            empHrs = 4;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    monthlyHours = monthlyHours + empHrs;
+                }
+                Monthly_Hours.Add(monthlyHours);
+                num_of_comp--;
+            }
+            //Console.WriteLine("The monthly hours of employee is : " + monthlyHours);
+            foreach (var item in Monthly_Hours)
+            {
+                Console.WriteLine("Company hours: " + item + " hrs ");
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Calculate the different company monthly hours daily wage is 100, part time hrs 4,full time hrs 8, abstract class with arraylist
+namespace ConsoleApp1
+{
+    internal class Program //: Implementatin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EMp Hours ");
+            Program program = new Program();
+            
+            Implementatin implementatin = new Implementatin();
+            implementatin.HoursPerMonth();
+
+        }
+    }
+}
